3e6e2b4a6f65f4df24c84b9b1f5ffb8681256b3f
saeed <saeed@Saeed>
2025-07-10 11:54:21 +0530

    User Def Exception

---
+++ DayEleven/src/com/tnsif/dayeleven/finallyblock/greatervalueException.java
@@ -0,0 +1,19 @@
+package com.tnsif.dayeleven.finallyblock;
+
+public class greatervalueException extends Exception {
+	
+	
+
+	public greatervalueException(String string)
+	{
+		super (string);
+	}
+	public greatervalueException()
+	{
+		super ("Percentage should not be more then 100");
+	}
+
+}
+	
+
+
